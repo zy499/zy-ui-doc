6607151230c3851c921957a15ad107bd690dfb36
zy499 <hqh499@gmail.com>
2020-03-11 12:42:23 +0800

    up

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,7 +6,7 @@ import hljs from 'highlight.js'
 import 'highlight.js/styles/googlecode.css' //样式文件
 
 import zyUi from 'zy499-ui' // 要编写对应的文档的包
-import 'zy499-ui/dist/zy-ui.css'
+import 'zy499-ui/dist/zy499-ui.css'
 Vue.directive('highlight',function (el) {
   let blocks = el.querySelectorAll('pre code');
   blocks.forEach((block)=>{
